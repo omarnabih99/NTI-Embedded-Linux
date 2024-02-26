b20fccf48a9aa2693c91269b4b69853e2168094d
Omar Nabih <omarnabih21@gmail.com>
2024-02-26 21:30:44 +0200

    Second Commit

---
+++ AdminLinux_Task1/Task1_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/
+mkdir myDirectory
+cd myDirectory/
+mkdir secondDirectory
+cd secondDirectory/
+touch myNotePaper
+cp ~/myDirectory/secondDirectory/myNotePaper ~/myDirectory/myOldNotePaper
