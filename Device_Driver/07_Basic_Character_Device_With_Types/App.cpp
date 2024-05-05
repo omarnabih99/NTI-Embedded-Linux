f310aba265b03fa0866e50ba07cc228e473f01ae
omarnabih99 <omarnabih21@gmail.com>
2024-05-05 16:35:12 +0300

    modify the app layer to support multiple devices

--- Device_Driver/07_Basic_Character_Device_With_Types/App.cpp
+++ Device_Driver/07_Basic_Character_Device_With_Types/App.cpp
@@ -4,17 +4,22 @@
 class Basic_Character_Devices
 {
     private:
-        const std::string PATH {"/dev/Basic_Device2"};
+        const std::string PATH;
         std::fstream fileDescriptor;
 
     public:
         void WriteFile (std::string message);
         std::string ReadFile (void);
+        Basic_Character_Devices(std::string devicePath);
 };
+
+Basic_Character_Devices::Basic_Character_Devices(std::string devicePath):PATH(devicePath){}
 
 void Basic_Character_Devices::WriteFile(std::string message)
 {
     fileDescriptor.open(PATH, std::ios::out);
+
+    message = message + "\n";
 
     fileDescriptor.write(message.c_str(), message.size());
 
@@ -36,17 +41,49 @@ std::string Basic_Character_Devices::ReadFile(void)
 
 int main ()
 {
-    std::string input;
-
-    Basic_Character_Devices Basic_Device2;
+    Basic_Character_Devices Basic_Device0("/dev/Basic_Device0"); //RDONLY
+    Basic_Character_Devices Basic_Device1("/dev/Basic_Device1"); //WRONLY
+    Basic_Character_Devices Basic_Device2("/dev/Basic_Device2"); //RDWR
+    Basic_Character_Devices Basic_Device3("/dev/Basic_Device3"); //RDWR
 
     std::cout << "The Application is running..." << "\n";
 
-    std::cout << "Please enter value: ";
+    std::string input;
 
-    std::getline(std::cin, input);
+    std::cout << "Please enter the desired device: ";
+    int desiredDevice;
+    std::cin >> desiredDevice;
+    switch (desiredDevice)
+    {
+        case 0:
+                std::cout << "Warning: This device is read-only" << "\n";
+                std::cout << Basic_Device0.ReadFile() << "\n";
+                break;
 
-    Basic_Device2.WriteFile(input);
+        case 1: 
+                std::cout << "Warning: This device is write-only" << "\n";
+                //std::cout << "Please enter value: ";
+                //std::getline(std::cin, input);
+                //Basic_Device1.WriteFile(input);
+                break;
 
-    std::cout << Basic_Device2.ReadFile() << "\n";
+        case 2:
+                std::cout << "Warning: This device is read-write" << "\n";
+                //std::cout << "Please enter value: ";
+                //std::getline(std::cin, input);
+                input = "This is device 2";
+                Basic_Device2.WriteFile(input);
+                std::cout << Basic_Device2.ReadFile() << "\n";
+                break;
+
+        case 3:
+                std::cout << "Warning: This device is read-write" << "\n";
+                //std::cout << "Please enter value: ";
+                //std::getline(std::cin, input);
+                input = "This is device 3";
+                Basic_Device3.WriteFile(input);
+                std::cout << Basic_Device3.ReadFile() << "\n";
+                break;
+    }
+
 }
